37698f034eb89a35a8301790eb7b09e000add412
Promise Fru <promisefru1@gmail.com>
2021-02-08 13:35:20 +0100

    contents instance variable created

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,13 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for key, value in kwargs.items():
+            for x in range(value):
+                self.contents.append(key)
 
+hat = Hat(red=4, black = 2)
+print(hat.contents)
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
